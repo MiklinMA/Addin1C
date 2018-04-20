5ad0e843432e100684e672f6de0b15708b9bfef4
Mikhail Miklin <MiklinMA@gmail.com>
2018-04-20 12:12:20 +0500

    Target platform

--- Nats/Source.cpp
+++ Nats/Source.cpp
@@ -11,4 +11,5 @@ Addin1C::Variant OnesNats::connect(Addin1C::VariantParameters& p) {
 	if (host.length() == 0) return std::wstring(L"Server HOST is not set.");
 	if (port == 0)			return std::wstring(L"Server PORT is not set.");
 
+	return (std::wstring) L"Connection thread started to " + host + L":" + std::to_wstring((long long)port);
 }
